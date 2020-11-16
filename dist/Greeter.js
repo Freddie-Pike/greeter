ee82f5def64b6cdc9718ce2afea27e40c1385123
Freddie Pike <Freddie@colabsoftware.com>
2020-11-16 16:32:46 -0330

    Release 0.0.1

---
+++ dist/Greeter.js
@@ -0,0 +1,11 @@
+// From the TypeScript docs: https://www.typescriptlang.org/docs/handbook/classes.html
+class Greeter {
+    constructor(message) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+export default Greeter;
+//# sourceMappingURL=Greeter.js.map
---
+++ dist/index.js
@@ -0,0 +1,2 @@
+export { default } from "./Greeter";
+//# sourceMappingURL=index.js.map
